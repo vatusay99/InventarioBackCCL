72e9045f3190114c3de46d4c34b8fbc46c8c9bb2
sinedin <desarrollador22@tecsersas.com>
2025-04-15 14:29:58 -0500

    Creacion de entidad Producto

---
+++ InventarioBackCCL/Models/Entities/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    internal class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int cantidad { get; set; }
+
+    }
+}
